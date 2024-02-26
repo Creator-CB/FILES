612704c96ce444dbde33d7431ac6e30d62f35986
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-02-26 13:47:41 +0200

    Create go1.sh
    Signed-off-by: Creator-CB <117445826+Creator-CB@users.noreply.github.com>

---
+++ go1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update
+sudo apt install mc jq curl build-essential git wget git lz4 -y
+sudo rm -rf /usr/local/go
+curl -Ls https://go.dev/dl/go1.21.7.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+
+cat <<'EOF' >>$HOME/.profile
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GO111MODULE=on
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+EOF
+
+source $HOME/.profile
+sleep 1
