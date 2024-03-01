72724aa5cf64ec9eabe57d51740c7bb75f96337c
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-03-01 16:14:53 +0200

    Update Foundrip.sh
    Signed-off-by: Creator-CB <117445826+Creator-CB@users.noreply.github.com>

--- Foundrip.sh
+++ Foundrip.sh
@@ -2,13 +2,10 @@
 
 set -e
 
-# Update and upgrade system packages
 apt-get update && sudo apt-get upgrade -y
 
-# Install required packages
 apt-get install curl jq -y
 
-# Retry the curl command a few times before giving up
 max_retries=3
 retry_delay=10
 retries=0
@@ -23,7 +20,6 @@ while [ $retries -lt $max_retries ]; do
     fi
 done
 
-# Check if installation was successful
 if [ $retries -eq $max_retries ]; then
     echo "Error: Failed to install Foundry after $max_retries attempts."
     exit 1
