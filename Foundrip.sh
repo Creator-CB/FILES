70a9f65214936a300f2d97a32479bee7322f3c76
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-03-01 16:03:02 +0200

    Update Foundrip.sh
    Signed-off-by: Creator-CB <117445826+Creator-CB@users.noreply.github.com>

--- Foundrip.sh
+++ Foundrip.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-set -e
-
-# Update and upgrade system packages
 apt-get update && sudo apt-get upgrade -y
 
-# Install required packages
 apt-get install curl jq -y
 
-# Run the installation script for FoundryP
 curl -L https://foundryp.paradigm.xyz | sudo bash
 
-# Source bashrc to apply changes
 source /root/.bashrc || source /etc/profile
 
-# Run foundryup command
 foundryup || source /root/.bashrc || source /etc/profile
