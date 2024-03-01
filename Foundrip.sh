63723254a2f5e7aa7e7eb664c7e7aba52673fac8
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-03-01 23:58:56 +0200

    Update Foundrip.sh
    Signed-off-by: Creator-CB <117445826+Creator-CB@users.noreply.github.com>

--- Foundrip.sh
+++ Foundrip.sh
@@ -13,11 +13,25 @@ max_retries=3
 retry_delay=10
 retries=0
 while [ $retries -lt $max_retries ]; do
-   if curl -L https://foundry.paradigm.xyz | sudo bash; then
+    if curl -L https://foundry.paradigm.xyz | sudo bash; then
         echo "Foundry installation successful."
         break
-   
+    else
+        echo "Failed to install Foundry. Retrying in $retry_delay seconds..."
+        sleep $retry_delay
+        retries=$((retries+1))
+    fi
+done
 
+# Check if installation was successful
+if [ $retries -eq $max_retries ]; then
+    echo "Error: Failed to install Foundry after $max_retries attempts."
+    exit 1
+fi
+
+# Pause after basic installation
+echo "Waiting for installation to complete..."
+sleep 10
 
 # Source bashrc to apply changes
 source /root/.bashrc || source /etc/profile
@@ -32,3 +46,4 @@ fi
 
 # Source bashrc again after foundryup
 source /root/.bashrc || source /etc/profile
+
